636dbb6eed31233aa598392381d101a63df50a2e
Mardali Tamalov <tamalov@softlab.ru>
2025-06-01 09:50:10 +0300

    добавил Swagger

--- src/main/java/com/example/usersubmanager/UserSubManagerApplication.java
+++ src/main/java/com/example/usersubmanager/UserSubManagerApplication.java
@@ -1,13 +1,28 @@
 package com.example.usersubmanager;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+//@EnableWebMvc
 public class UserSubManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserSubManagerApplication.class, args);
     }
 
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .openapi("3.0.1")
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("API documentation"));
+    }
 }
--- src/main/java/com/example/usersubmanager/controller/UsersController.java
+++ src/main/java/com/example/usersubmanager/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.example.usersubmanager.controller;
 
 import com.example.usersubmanager.dto.UserRequestDto;
 import com.example.usersubmanager.dto.UserResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 public interface UsersController {
@@ -9,6 +10,7 @@ public interface UsersController {
     @PostMapping()
     public UserResponseDto addUsers(@RequestBody UserRequestDto userRequestDto);
 
+    @Operation(summary = "получение пользователя по id")
     @GetMapping("/{id}")
     public UserResponseDto getUser(@PathVariable Long id);
 
